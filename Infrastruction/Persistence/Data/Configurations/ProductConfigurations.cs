e763c9607c83f152d2ffbf331d556e0c7b5dfb56
AhmedHaany <shmed2001@gmail.com>
2025-02-02 14:50:38 +0200

    Product Module

---
+++ Infrastruction/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Persistence.Data.Configurations
+{
+	internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.HasOne(Product => Product.ProductBrand)
+				.WithMany()
+				.HasForeignKey(Product => Product.BrandId);
+
+			builder.HasOne(Product => Product.ProductType)
+				.WithMany()
+				.HasForeignKey(Product => Product.TypeId);
+
+			builder.Property(Product => Product.Price)
+				.HasColumnType("decimal(18,3)");
+		}
+	}
+}
